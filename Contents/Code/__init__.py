aa4a3522b89727fdace811862982bc5271072af2
Mike <mike.d.meier@gmail.com>
2012-04-09 18:56:33 +0000

    Send the username and password headers when trying to retrieve the API Key. Dumbass!

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -42,7 +42,7 @@ def AuthHeader():
 ####################################################################################################
 
 def MainMenu():
-    oc = ObjectContainer()
+    oc = ObjectContainer(no_cache=True)
     
     API_KEY = False
     if Dict['SB_API_Key']:
@@ -567,7 +567,7 @@ def API_URL():
 def Get_API_Key():
     '''scrape the SickBeard/Config/General page for the API key and set it in the plugin Dict[]'''
     url = Get_SB_URL() + '/config/general'
-    page = HTML.ElementFromURL(url, cacheTime=0)
+    page = HTML.ElementFromURL(url, headers=AuthHeader() cacheTime=0)
     try:
         api_key = page.xpath('//input[@name="api_key"]')[0].get('value')
         Log("API key found.")
